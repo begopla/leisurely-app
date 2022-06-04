020d947289a0cf69ed40004ff1e9497d2bb10ecc
Lukas Baur <baurlukas89@gmail.com>
2022-06-04 15:33:15 +0200

    comments not posting bug-fix

--- routes/activity.routes.js
+++ routes/activity.routes.js
@@ -135,7 +135,11 @@ router.post("/:id/save", isLoggedIn, async (req, res, next) => {
       { $addToSet: { bookmarkList: savedActivity._id } },
       { new: true }
     );
-    await Activity.findByIdAndUpdate(id,{$addToSet:{savedByUsers: currentUserId}}, {new: true});
+    await Activity.findByIdAndUpdate(
+      id,
+      { $addToSet: { savedByUsers: currentUserId } },
+      { new: true }
+    );
     res.redirect("/profile/savedactivities");
   } catch (error) {
     next(error);
@@ -157,7 +161,11 @@ router.post("/:id/unsave", isLoggedIn, async (req, res, next) => {
       { new: true }
     );
 
-    await Activity.findByIdAndUpdate(id,{$pull:{savedByUsers: currentUserId}}, {new: true});
+    await Activity.findByIdAndUpdate(
+      id,
+      { $pull: { savedByUsers: currentUserId } },
+      { new: true }
+    );
 
     // await Activity.findByIdAndUpdate(id,{$addToSet:{savedByUsers: id}}, {new: true});
     res.redirect("/profile");
@@ -169,40 +177,50 @@ router.post("/:id/unsave", isLoggedIn, async (req, res, next) => {
 router.get("/:id", async (req, res, next) => {
   try {
     const { id } = req.params;
-    const activityDetails = await Activity.findById(id).populate("user").populate("savedByUsers").populate("comments"); 
-    const comments = activityDetails.comments;
-    //console.log(activityDetails,comments)                                 
-    const theUser = req.session.currentUser;  
-    let notsavedactivity= true;
+    const activityDetails = await Activity.findById(id)
+      .populate("user")
+      .populate("savedByUsers")
+      .populate({
+        path: "comments",
+        populate: ["user"],
+      });
+    /* const comments = activityDetails.comments;
+    console.log(activityDetails.comments); */
+    const theUser = req.session.currentUser;
+    let notsavedactivity = true;
     if (theUser) {
       const activityUser = activityDetails.user;
       const activityUserId = activityUser._id.valueOf();
       const currentUserId = theUser._id;
       const savedusers = activityDetails.savedByUsers;
       let savedactivities = false;
-      savedusers.forEach(element => {
-        if(currentUserId === element._id.valueOf() ){
-           savedactivities =true;
-           notsavedactivity = false;
-           console.log('eureka!')
-        };  
+      savedusers.forEach((element) => {
+        if (currentUserId === element._id.valueOf()) {
+          savedactivities = true;
+          notsavedactivity = false;
+          console.log("eureka!");
+        }
       });
-  
-               
-      if( activityUserId === currentUserId){
+
+      if (activityUserId === currentUserId) {
         res.render("activities/activities-details", {
           activityDetails,
-          theUser, savedactivities, notsavedactivity
-          })
-           
-           
-      }else{
-         res.render("activities/activities-details", { activityDetails,notsavedactivity,savedactivities });
-        }
-
+          theUser,
+          savedactivities,
+          notsavedactivity,
+        });
+      } else {
+        res.render("activities/activities-details", {
+          activityDetails,
+          notsavedactivity,
+          savedactivities,
+        });
+      }
     } else {
-      res.render("activities/activities-details",  { activityDetails, notsavedactivity });
-
+      res.render("activities/activities-details", {
+        activityDetails,
+        notsavedactivity,
+      });
     }
   } catch (error) {
     next(error);
