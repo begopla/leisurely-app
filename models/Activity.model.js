6af33732df9a5f498dc57d93ce38f4e75abb274e
Lukas Baur <baurlukas89@gmail.com>
2022-06-07 23:10:34 +0200

    added searchbar

--- models/Activity.model.js
+++ models/Activity.model.js
@@ -22,10 +22,12 @@ const activitySchema = new Schema(
       type: Schema.Types.ObjectId,
       ref: "User",
     },
-    comments: [{
-      type: Schema.Types.ObjectId,
-      ref: "Comment",
-    }],
+    comments: [
+      {
+        type: Schema.Types.ObjectId,
+        ref: "Comment",
+      },
+    ],
     savedByUsers: [
       {
         type: Schema.Types.ObjectId,
@@ -37,5 +39,11 @@ const activitySchema = new Schema(
     timestamps: true,
   }
 );
+
+activitySchema.index({
+  name: "text",
+  location: "text",
+});
+
 const Activity = model("Activity", activitySchema);
 module.exports = Activity;
