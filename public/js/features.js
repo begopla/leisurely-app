ae2155c7f55d76a4db6dcec8e4cfb66b3488aa3c
Lukas Baur <baurlukas89@gmail.com>
2022-06-08 19:28:44 +0200

    added form feature

---
+++ public/js/features.js
@@ -0,0 +1,17 @@
+/* const inputEl = document.querySelectorAll(".form-control");
+const labelEl = document.querySelectorAll(".form-label");
+
+inputEl.addEventListener("focus", () => labelEl.classList.add("hidden"));
+inputEl.addEventListener("blur", () => labelEl.classList.remove("hidden")); */
+
+const allLabels = document.querySelectorAll(".form label");
+
+allLabels.forEach((label) => {
+  label.innerHTML = label.innerText
+    .split("")
+    .map(
+      (letter, index) =>
+        `<span style="transition-delay: ${index * 80}ms">${letter}</span>`
+    )
+    .join("");
+});
